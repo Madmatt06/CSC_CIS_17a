e2afee7c2f53f6fc7a23c3ea9d6617cd19580345
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2025-09-03 12:15:52 -0700

    Completed Hello World program

---
+++ Lab/IntroLab/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Matthew Madrigal
+ * Created on September 3, 2025, 12:02 PM
+ * Purpose: A simple hello world program
+ */
+
+//System Libraries
+#include <iostream> 
+using namespace std;
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    
+    //Prints Hello World
+    cout << "Hello World!" << endl;
+
+    //Exit the Program
+    return 0;
+}
