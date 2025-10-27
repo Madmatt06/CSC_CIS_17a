2e5277b23dc3905343f69a8a64afab93d89d922e
Madmatt06 <55221261+Madmatt06@users.noreply.github.com>
2025-10-26 17:47:14 -0700

    Added files for next assignments

---
+++ Hmwk/Assignment 4/Gaddis_9thEd_Chap12_Prob7_Sentence_Filter/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Matthew Madrigal
+ * Created on October 26th, 2025, 5:44 pm,
+ * Purpose:  To take in a file and capitalize the first letter in every sentence while making everything else lowercase.
+ */
+
+//System Libraries
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math/Science/Conversions only
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    fstream input, output;
+    //Initialize Variables
+    
+    //The Process -> Map Inputs to Outputs
+    
+    //Display Inputs/Outputs
+
+    //Exit the Program
+    return 0;
+}
