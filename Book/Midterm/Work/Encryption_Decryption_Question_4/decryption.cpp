d0163fa51cbd5a6ecb1c905eaee14c866c9364ee
Madmatt06 <55221261+Madmatt06@users.noreply.github.com.>
2025-11-02 21:17:07 -0800

    Finished Cleaning up and adding comments for decryption file

--- Book/Midterm/Work/Encryption_Decryption_Question_4/decryption.cpp
+++ Book/Midterm/Work/Encryption_Decryption_Question_4/decryption.cpp
@@ -20,17 +20,18 @@ bool valMessMin(string);
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare Variables
-    const int SIZE = 4;
+    const int SIZE = 4; // Only 4 digits
     int message[SIZE];
-    string input;
-    bool valid;
-    int temp;
+    string input;       // Stores user input to be validated
+    bool valid;         // Stores if input is valid
     
     //Initialize Variables
+    // Gets a message to decrypt
     do {
         cout << "Enter your message to decrypt: ";
         cin >> input;
         valid = valMessage(input);
+        // If the first function returned false, check if its due to corruption during transmission or user error
         if(!valid) {
             if(!valMessMin(input)) cout << "Invalid message entered." << endl;
             else cout << "The message seems to have been corrupted during transmission due to an 8 or 9 existing in it." << endl;
@@ -38,20 +39,24 @@ int main(int argc, char** argv) {
     } while(!valid);
     
     //The Process -> Map Inputs to Outputs
+    // Converts to ints
     for(int i = 0; i < SIZE; i++) {
         message[i] = input[i] - '0';
     }
+
+    // Swaps the places
     for(int i = 0; i < SIZE/2; i++) {
-        temp = message[i];
+        int temp = message[i];
         message[i] = message[(SIZE/2)+i];
         message[(SIZE/2)+i] = temp;
     }
+
+    // Performs the operations to reverse it. (If its greater than 2, subtract 3, otherwise add 5. Thats the best way I could figure out reversing modulus)
     for(int i = 0; i < SIZE; i++) {
         if(message[i] > 2) message[i] -= 3;
         else message[i] += 5;
     }
 
-    
     //Display Inputs/Outputs
     cout << endl << "Decrypted Message: ";
     for(int i = 0; i < SIZE; i++) {
@@ -63,6 +68,7 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+// Validates the user input is valid (multiple digits all between 0 and 7 inclusive)
 bool valMessage(string input) {
     if(input.length() != 4) return false;
     for(int i = 0; i < input.length(); i++) {
