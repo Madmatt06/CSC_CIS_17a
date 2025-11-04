ef6ce8b7adbfdaface4f7995938fa93d4d7a14d8
Madmatt06 <55221261+Madmatt06@users.noreply.github.com.>
2025-11-03 17:47:48 -0800

    Forgot Address in banking question

--- Book/Midterm/Work/Banking_Question_1/main.cpp
+++ Book/Midterm/Work/Banking_Question_1/main.cpp
@@ -148,6 +148,7 @@ int main(int argc, char** argv) {
     cout << "The following data has been entered." << endl;
     cout << "Account number: " << user->accnum << endl;
     cout << "Name: " << user->name << endl;
+    cout << "Address: " << user->addr << endl;
     cout << "Starting balance: $" << fixed << setprecision(2) << user->bal << endl;
     cout << "Number of checks written: " << user->drSize << endl;
     cout << "Check amounts written: ";
